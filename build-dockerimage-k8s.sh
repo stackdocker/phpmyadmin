31bc35722ce4b950d0790f631d94a79cae6b2cfd
tangfeixiong <tangfx128@gmail.com>
2016-06-14 00:52:37 +0000

    kubernetes service auto discovery version

---
+++ build-dockerimage-k8s.sh
@@ -0,0 +1,34 @@
+#!/bin/bash -e
+
+src=$(dirname "${BASH_SOURCE}")
+tgt=$(mktemp -d)
+
+cp -r $src $tgt
+cp $src/configK8s.inc.php $tgt/config.inc.php
+
+cat <<DF > ${tgt}/Dockerfile.k8s
+# Switch back to stable once 3.4 is out
+FROM alpine:edge
+
+RUN apk add --no-cache php5-cli php5-mysqli php5-ctype php5-xml php5-gd php5-zlib php5-bz2 php5-zip php5-openssl php5-curl php5-opcache php5-json php5-mcrypt curl
+
+RUN curl --location https://www.phpmyadmin.net/downloads/phpMyAdmin-latest-all-languages.tar.gz | tar xzf - \
+ && mv phpMyAdmin* /www \
+ && rm -rf /www/js/jquery/src/ /www/examples /www/po/
+
+COPY config.inc.php /www/
+COPY run.sh /run.sh
+RUN chmod u+rwx /run.sh
+
+VOLUME /sessions
+
+EXPOSE 80
+
+ENV PHP_UPLOAD_MAX_FILESIZE=64M \
+    PHP_MAX_INPUT_VARS=2000
+
+ENTRYPOINT [ "/run.sh" ]
+DF
+
+docker build -t tangfeixiong/phpmyadmin:k8s $tgt
+#rm -rf $tgt
